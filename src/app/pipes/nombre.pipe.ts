ed363a981ccbeff5a79a9f7bcd1ac184f745f2cc
TrittoG <36174147+TrittoG@users.noreply.github.com>
2019-12-05 20:59:30 -0300

    Update nombre.pipe.ts

--- src/app/pipes/nombre.pipe.ts
+++ src/app/pipes/nombre.pipe.ts
@@ -6,7 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NombrePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    return null;
+    if(value == "")
+    {
+      return "Sin Nombre";
+    }
   }
 
 }
